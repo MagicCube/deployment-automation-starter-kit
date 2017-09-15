af95f5f819ba2f4547a9108e2ac25a32bb72c08f
Henry Li <henry1943@163.com>
2017-09-15 15:11:32 +0800

    Add 'da'

---
+++ src/da/components/App/index.jsx
@@ -0,0 +1,38 @@
+import PropTypes from 'prop-types';
+import React, { PureComponent } from 'react';
+
+import BuildInfo from '../BuildInfo';
+
+import styles from './index.less';
+
+export default class App extends PureComponent {
+  static propTypes = {
+    lastBuild: PropTypes.shape().isRequired
+  }
+
+  renderBuildSection() {
+    const lastBuild = this.props.lastBuild;
+    return (
+      <section>
+        <h3>Build Info</h3>
+        <BuildInfo build={lastBuild} />
+      </section>
+    );
+  }
+
+  render() {
+    return (
+      <div className={styles.container}>
+        <h1>Welcome to deployment-automation</h1>
+        {this.renderBuildSection()}
+        <section>
+          <h3>About</h3>
+          <div>
+            For more details, please visit&nbsp;
+            <a href="https://zhuanlan.zhihu.com/p/29231319/edit">https://zhuanlan.zhihu.com/p/29231319/edit</a>
+          </div>
+        </section>
+      </div>
+    );
+  }
+}
